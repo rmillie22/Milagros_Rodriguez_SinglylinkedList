5b8c3a1ce4b8c26ea09b8f3a7a06eeb52914f19f
Milagros D Rodriguez <50366134+rmillie22@users.noreply.github.com>
2019-06-24 15:57:19 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+import javax.xml.soap.Node;
+
+public class SinglyLinkedList {
+    public Node head;
+    public SinglyLinkedList() {
+        this.head = null;
+    }
+    // SLL methods go here. As a starter, we will show you how to add a node to the list.
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if(head == null) {
+            head = newNode;
+        } else {
+            Node runner = head;
+            while(runner.next != null) {
+                runner = runner.next;
+            }
+            runner.next = newNode;
+        }
+    }
+    public void remove(){
+        Node runner = this.head;
+        while(runner.next.next != null){
+            runner = runner.next;
+        }
+        runner.next = null;
+    }
+    public void printValues(){
+        Node runner = this.head;
+        if(runner == null){
+            System.out.println(runner);
+        } else {
+            while(runner != null){
+                System.out.println(runner.value);
+                runner = runner.next;
+            }
+        }
+    }
+}
